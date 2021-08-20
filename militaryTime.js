ba3bba16d098acb3bb61c85236a1a310adfe60bf
Misbah Ahmed <72851859+misbahofficial@users.noreply.github.com>
2021-08-20 11:24:36 +0600

    Update militaryTime.js

--- militaryTime.js
+++ militaryTime.js
@@ -1,9 +1,9 @@
 
 // prompt-sync, a node module for taking user input in
 var ps = require("prompt-sync");
-
 var input = ps();
 
+// defining a print function to simplify the console.log to see output in the console
 function print(value){
     console.log(value);
 }
@@ -13,9 +13,9 @@ function print(value){
 
 var userInput = input("Enter a 12 hour format time: ");
 
-var timeAndAmOrPm = userInput.split(" ");
+var timeAndAmOrPm = userInput.split(" "); // splitting the actual string on the basis of space
 var temp = timeAndAmOrPm[0];
-var hourMin = temp.split(":");
+var hourMin = temp.split(":"); // spliting the sub-string on the basis of colon
 var hoursInt = parseInt(hourMin[0]);
 
 
