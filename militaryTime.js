0b8dd8e3383a9a4e077289f4b1dbcf137d886436
Misbah Ahmed <72851859+misbahofficial@users.noreply.github.com>
2021-08-21 18:45:57 +0600

    Update militaryTime.js

--- militaryTime.js
+++ militaryTime.js
@@ -13,26 +13,26 @@ function print(value){
 
 var userInput = input("Enter a 12 hour format time: ");
 
-var timeAndAmOrPm = userInput.split(" "); // splitting the actual string on the basis of space
+var timeAndAmOrPm = userInput.split(" ");
 var temp = timeAndAmOrPm[0];
-var hourMin = temp.split(":"); // spliting the sub-string on the basis of colon
+var hourMin = temp.split(":");
 var hoursInt = parseInt(hourMin[0]);
-
+hoursInt = hoursInt + 12;
+var hourString = hoursInt.toString();
 
 if (timeAndAmOrPm[1] == 'AM') {
-    print(userInput);
-} else {
-    hoursInt = hoursInt + 12;
-    var hourString = hoursInt.toString();
 
     if (hourString == '24') {
-
         var finalTime = "00" + ":" + hourMin[1] + " AM";
         print(finalTime);
-
     } else {
-        var finalTime = hourString + ":" + hourMin[1] + " PM";
-        print(finalTime);
+        print(userInput);
     }
 
+} else {
+
+        var finalTime = hourString + ":" + hourMin[1] + " PM";
+        print(finalTime);
+    
 }
+
